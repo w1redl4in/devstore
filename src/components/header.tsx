9c24eda232f788b71621f44b088d89b270792901
Felipe <felipe.austriaco@mercadolivre.com>
2024-01-25 17:09:59 -0300

    style(components): :lipstick: styling header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,44 @@
+import Link from 'next/link'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+
 export function Header() {
-  return <div>header</div>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="w-5 h-5 text-zinc-500" />
+
+          <input
+            type="text"
+            placeholder="Buscar produtos..."
+            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500 "
+          />
+        </form>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-4 w-4" />
+          <span>Cart (0)</span>
+        </div>
+
+        <div className="w-px h-4 bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Conta</span>
+          <Image
+            alt=""
+            width={24}
+            height={24}
+            src="https://github.com/w1redl4in.png"
+            className="h-6 w-6 rounded-full border-2 border-zinc-700"
+          />
+        </Link>
+      </div>
+    </div>
+  )
 }

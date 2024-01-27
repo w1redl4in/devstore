cbb1df1a7ac7218027df17178cb05f5a841b0498
Felipe <felipe.austriaco@mercadolivre.com>
2024-01-27 10:35:08 -0300

    feat(home): :sparkles: add home loading page with skeletons

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,7 +1,11 @@
-export default function Loading() {
+import { Skeleton } from '@/components/skeleton'
+
+export default function HomeLoading() {
   return (
-    <div>
-      <h1>Loading home..</h1>
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[856px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
     </div>
   )
 }

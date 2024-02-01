feb07e836d42396b5ec022bbbd1701bf537c5245
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-01 20:35:36 -0300

    feat(layout): :sparkles: add dynamic title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,10 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: 'devstore',
+  title: {
+    template: '%s | devstore',
+    default: 'devstore',
+  },
   description: 'Curso de next14 com testes',
 }
 

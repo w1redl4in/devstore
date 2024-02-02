d85aa2ac495a15677d53970bc9be89a8571df458
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-02 01:01:07 -0300

    feat(layout): :sparkles: add a suspense on layout

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,14 +1,16 @@
 import { Header } from '@/components/header'
 import { CartProvider } from '@/contexts/cart'
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <CartProvider>
-      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-        <Header />
-        {children}
-      </div>
-    </CartProvider>
+    <Suspense>
+      <CartProvider>
+        <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+          <Header />
+          {children}
+        </div>
+      </CartProvider>
+    </Suspense>
   )
 }

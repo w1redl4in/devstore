07f13e2f127fe8c8d743f334ca3e80906dd54354
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-01 21:59:09 -0300

    feat(route-search): :sparkles: add endpoint for search products by query params

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+import data from '../data.json'
+import type { NextRequest } from 'next/server'
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+
+  const products = data.products.filter((product) =>
+    product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
+  )
+
+  return Response.json(products)
+}

079790cdabb2804ec16d45f603801c317211cf58
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-01 21:51:58 -0300

    refactor: :recycle: abstracting components that need context for client components

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { api } from '@/app/data/api'
 import { Product } from '@/app/data/types/product'
 import { formatCurrencyInBRL } from '@/app/utils/format-currency'
+import { AddToCartButton } from '@/components/add-to-cart-button'
 import { Metadata } from 'next'
 import Image from 'next/image'
 
@@ -69,7 +70,7 @@ export default async function ProductPage({ params }: ProductProps) {
             {formatCurrencyInBRL(product.price)}
           </span>
           <span className="text-sm text-zinc-400">
-            {formatCurrencyInBRL(product.price / 12)}
+            Em até 12/s juros de {formatCurrencyInBRL(product.price / 12)}
           </span>
         </div>
 
@@ -91,12 +92,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
---
+++ src/components/add-to-cart-button.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useCart } from '@/contexts/cart'
+
+interface AddToCartButtonProps {
+  productId: number
+}
+export function AddToCartButton({ productId }: AddToCartButtonProps) {
+  const { addToCart } = useCart()
+
+  function handleAddToCart() {
+    addToCart(productId)
+  }
+
+  return (
+    <button
+      onClick={handleAddToCart}
+      type="button"
+      className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
+    >
+      Adicionar ao carrinho
+    </button>
+  )
+}
